db55eb9f788e1a4726ad655481f36b3f299bf237
TechnOllieG <56596714+TechnOllieG@users.noreply.github.com>
2020-04-02 17:08:01 +0200

    Finalized Case File

--- Assets/Scripts/BookHoverController.cs
+++ Assets/Scripts/BookHoverController.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class BookHoverController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject mainObject;
+    public GameObject displayObject;
+    public GameObject laterMain;
+    public GameObject laterDisplay;
+    public GameObject earlierMain;
+    public GameObject earlierDisplay;
 
-    // Update is called once per frame
+    public float closedDisableHover;
+    public float openDisableHover;
+
     void Update()
     {
+        displayObject.transform.rotation = mainObject.transform.rotation;
         
+        if (mainObject.transform.localEulerAngles.x > closedDisableHover)
+        {
+            laterMain.SetActive(true);
+            laterDisplay.SetActive(false);
+        }
+        else
+        {
+            laterMain.SetActive(false);
+            laterDisplay.SetActive(true);
+        }
+        
+        if(mainObject.transform.localEulerAngles.x < openDisableHover)
+        {
+            earlierMain.SetActive(true);
+            earlierDisplay.SetActive(false);
+        }
+        else
+        {
+            earlierMain.SetActive(false);
+            earlierDisplay.SetActive(true);
+        }
     }
 }
