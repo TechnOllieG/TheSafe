51e999543ba9ab4ae8774978895600ce0f5cb6a6
TechnOllieG <56596714+TechnOllieG@users.noreply.github.com>
2020-04-11 17:13:16 +0200

    Added Paintings to House
    Also Gogh painting with math problem on back

---
+++ Assets/Scripts/SnapToWall.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+[RequireComponent(typeof(Interactable))]
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Throwable))]
+public class SnapToWall : MonoBehaviour
+{
+    public float snapRadius = 0.05f;
+    public bool showHint = true;
+    
+    public GameObject snapToObject;
+    [HideInInspector]
+    public MeshRenderer snapToMeshRenderer;
+
+    private bool withinRadius = false;
+    private bool holding = false;
+    void Start()
+    {
+        snapToMeshRenderer = snapToObject.GetComponent<MeshRenderer>();
+    }
+    void Update()
+    {
+        if(holding)
+        {
+            if (Mathf.Abs(transform.position.x - snapToObject.transform.position.x) <= snapRadius || Mathf.Abs(transform.position.y - snapToObject.transform.position.y) <= snapRadius || Mathf.Abs(transform.position.z - snapToObject.transform.position.z) <= snapRadius)
+            {
+                withinRadius = true;
+                snapToMeshRenderer.enabled = true;
+            }
+            else
+            {
+                withinRadius = false;
+                snapToMeshRenderer.enabled = false;
+            }
+        }
+    }
+    private void OnAttachedToHand()
+    {
+        holding = true;
+    }
+
+    private void OnDetachedFromHand()
+    {
+        if(withinRadius)
+        {
+            transform.position = snapToObject.transform.position;
+            transform.rotation = snapToObject.transform.rotation;
+        }
+        snapToMeshRenderer.enabled = false;
+        holding = false;
+    }
+}
