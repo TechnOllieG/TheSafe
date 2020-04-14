8399b7d63124ae707f66f2ffd54b02f123d827f5
TechnOllieG <56596714+TechnOllieG@users.noreply.github.com>
2020-04-14 16:26:14 +0200

    Fixed Morse Code Script

--- Assets/Scripts/LightController.cs
+++ Assets/Scripts/LightController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class LightController : MonoBehaviour
 {
@@ -7,6 +8,8 @@ public class LightController : MonoBehaviour
     public GameObject normalLight;
     public GameObject morseLight;
     public GameObject mathNote;
+    [Tooltip("Time Multiplier for the morse code 1.0: 1 unit = 1 second. 0.5: 1 unit = 0.5 seconds")]
+    public float morseTimeMultiplier = 1.0f;
 
     private SwitchController mainScript;
     private SwitchController secretScript;
@@ -25,14 +28,28 @@ public class LightController : MonoBehaviour
             {
                 normalLight.SetActive(false);
             }
+            if(morseLight.activeSelf && !mainScript.active || !secretScript.active)
+            {
+                morseLight.SetActive(false);
+            }
             if (mainScript.active && secretScript.active)
             {
                 morseActive = true;
-                Morse();
+                normalLight.SetActive(false);
+                StartCoroutine(Morse());
             }
-            else if (mainScript.active)
+            else if (mainScript.active && !normalLight.activeSelf)
             {
                 normalLight.SetActive(true);
+            }
+        }
+        else
+        {
+            if(!mainScript.active || !secretScript.active)
+            {
+                morseActive = false;
+                StopAllCoroutines();
+                MorseOff();
             }
         }
     }
@@ -40,13 +57,70 @@ public class LightController : MonoBehaviour
     {
         normalLight.SetActive(false);
         mathNote.SetActive(true);
+        while(morseActive)
+        {
+            yield return StartCoroutine(G());
+            yield return new WaitForSeconds(3 * morseTimeMultiplier);
+            yield return StartCoroutine(O());
+            yield return new WaitForSeconds(3 * morseTimeMultiplier);
+            yield return StartCoroutine(G());
+            yield return new WaitForSeconds(3 * morseTimeMultiplier);
+            yield return StartCoroutine(H());
+            yield return new WaitForSeconds(7 * morseTimeMultiplier);
+        }
+    }
+    IEnumerator G()
+    {
+        MorseOn();
+        yield return new WaitForSeconds(3 * morseTimeMultiplier);
+        MorseOff();
+        yield return new WaitForSeconds(1 * morseTimeMultiplier);
+        MorseOn();
+        yield return new WaitForSeconds(3 * morseTimeMultiplier);
+        MorseOff();
+        yield return new WaitForSeconds(1 * morseTimeMultiplier);
+        MorseOn();
+        yield return new WaitForSeconds(1 * morseTimeMultiplier);
+        MorseOff();
+    }
+    IEnumerator O()
+    {
+        MorseOn();
+        yield return new WaitForSeconds(3 * morseTimeMultiplier);
+        MorseOff();
+        yield return new WaitForSeconds(1 * morseTimeMultiplier);
+        MorseOn();
+        yield return new WaitForSeconds(3 * morseTimeMultiplier);
+        MorseOff();
+        yield return new WaitForSeconds(1 * morseTimeMultiplier);
+        MorseOn();
+        yield return new WaitForSeconds(3 * morseTimeMultiplier);
+        MorseOff();
+    }
+    IEnumerator H()
+    {
+        MorseOn();
+        yield return new WaitForSeconds(1 * morseTimeMultiplier);
+        MorseOff();
+        yield return new WaitForSeconds(1 * morseTimeMultiplier);
+        MorseOn();
+        yield return new WaitForSeconds(1 * morseTimeMultiplier);
+        MorseOff();
+        yield return new WaitForSeconds(1 * morseTimeMultiplier);
+        MorseOn();
+        yield return new WaitForSeconds(1 * morseTimeMultiplier);
+        MorseOff();
+        yield return new WaitForSeconds(1 * morseTimeMultiplier);
+        MorseOn();
+        yield return new WaitForSeconds(1 * morseTimeMultiplier);
+        MorseOff();
     }
     void MorseOn()
     {
-
+        morseLight.SetActive(true);
     }
     void MorseOff()
     {
-
+        morseLight.SetActive(false);
     }
 }
