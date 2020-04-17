a39c9d64ea9e724617bea26605ea0730ecd7b8a8
TechnOllieG <56596714+TechnOllieG@users.noreply.github.com>
2020-04-17 16:57:38 +0200

    Added Logic to Puzzle Box (Game now pretty much playable)

---
+++ Assets/PuzzleButton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PuzzleButton : MonoBehaviour
+{
+    public GameObject puzzleController;
+    public Material activeButton;
+    [Tooltip("Starts from 0, index 0 = Light 1 etc.")]
+    public int[] connectedLightsIndex;
+
+    private PuzzleBox puzzleBox;
+    private Material oldMaterial;
+    private MeshRenderer meshRenderer;
+    private bool active = false;
+
+    void Start()
+    {
+        puzzleBox = puzzleController.GetComponent<PuzzleBox>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        oldMaterial = meshRenderer.material;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(!puzzleBox.solved)
+        {
+            if (active)
+            {
+                active = false;
+                meshRenderer.material = oldMaterial;
+            }
+            else
+            {
+                active = true;
+                meshRenderer.material = activeButton;
+            }
+            puzzleBox.UpdateLights(connectedLightsIndex);
+        }
+    }
+}
---
+++ Assets/Scripts/PuzzleBox.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Linq;
+
+public class PuzzleBox : MonoBehaviour
+{
+    [Tooltip("Should be bound to the text object to be displayed on the screen.")]
+    public GameObject displayText;
+    [Tooltip("Should be bound to the material to display when the light is on")]
+    public Material litLight;
+
+    public GameObject[] lights;
+    private MeshRenderer[] lightRenderers;
+
+    private bool[] activeLights;
+    private Material oldLightMaterial;
+    [HideInInspector]
+    public bool solved = false;
+
+    private void Start()
+    {
+        displayText.SetActive(false);
+        lightRenderers = new MeshRenderer[lights.Length];
+        activeLights = new bool[lights.Length];
+
+        for(int i = 0; i < lights.Length; ++i)
+        {
+            lightRenderers[i] = lights[i].GetComponent<MeshRenderer>();
+        }
+
+        oldLightMaterial = lightRenderers[0].material;
+
+        for (int i = 0; i < activeLights.Length; ++i)
+        {
+            activeLights[i] = false;
+        }
+    }
+
+    public void UpdateLights(int[] lightIndex)
+    {
+        foreach (int element in lightIndex)
+        {
+            if(activeLights[element])
+            {
+                lightRenderers[element].material = oldLightMaterial;
+                activeLights[element] = false;
+            }
+            else
+            {
+                lightRenderers[element].material = litLight;
+                activeLights[element] = true;
+            }
+        }
+    }
+    private void Update()
+    {
+        if(activeLights.Contains(false))
+        {
+        }
+        else if(!solved)
+        {
+            solved = true;
+            displayText.SetActive(true);
+        }
+    }
+}
