b1fd19894a53f2ed82ba3bae7e163713eb07b14d
TechnOllieG <56596714+TechnOllieG@users.noreply.github.com>
2020-04-22 23:21:25 +0200

    Game Done for Initial Alpha Release

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,14 +1,24 @@
 ﻿using UnityEngine.Audio;
 using System;
+using System.Collections;
 using UnityEngine;
+using Valve.VR;
 
 public class AudioManager : MonoBehaviour
 {
+    public SteamVR_Action_Boolean headsetOnHead;
+    [Tooltip("How many seconds after putting on the hmd should the intro play?")]
+    public float secsToPlayIntro = 5.0f;
+    public float secsToPlayOpenSafe = 2.0f;
     public Sound[] sounds;
 
     [HideInInspector]
     public AudioManager instance;
 
+    private bool initIntroCall = false;
+    private bool initOpenSafeCall = false;
+    private bool headsetOnHeadBool;
+    private Sound nowPlaying;
     void Awake()
     {
         if(instance == null)
@@ -31,17 +41,50 @@ public class AudioManager : MonoBehaviour
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
+            s.source.playOnAwake = false;
+        }
+    }
+
+    private void Update()
+    {
+        if(headsetOnHead.stateDown && !initIntroCall)
+        {
+            StartCoroutine(IntroCallRoutine());
+            initIntroCall = true;
         }
     }
 
     public void Play(string name)
     {
-        Sound s = Array.Find(sounds, sound => sound.name == name);
-        if(s == null)
+        nowPlaying = Array.Find(sounds, sound => sound.name == name);
+        if(nowPlaying == null)
         {
             Debug.LogWarning("Sound: " + name + " was not found");
             return;
         }
-        s.source.Play();
+        Debug.Log("Playing sound with name: " + name);
+        nowPlaying.source.Play();
+    }
+    private IEnumerator IntroCallRoutine()
+    {
+        yield return new WaitForSeconds(secsToPlayIntro);
+        Play("IntroCall");
+    }
+    public void OpenSafeCall()
+    {
+        if(!initOpenSafeCall)
+        {
+            initOpenSafeCall = true;
+            StartCoroutine(OpenSafeCallRoutine());
+        }
+    }
+    private IEnumerator OpenSafeCallRoutine()
+    {
+        yield return new WaitForSeconds(secsToPlayOpenSafe);
+        if(nowPlaying.source.isPlaying)
+        {
+            nowPlaying.source.Stop();
+        }
+        Play("OpenSafeCall");
     }
 }
---
+++ Assets/Scripts/Incinerator.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Incinerator : MonoBehaviour
+{
+    [Tooltip("The incremental degree that the door will close with every frame update")]
+    public float doorIncrement;
+    public float timeUntilLightsOn;
+
+    public GameObject button;
+    public GameObject syringe;
+    public GameObject syringeSnapTo;
+    public GameObject lights;
+    public GameObject leftDoor;
+    public GameObject leftDoorClosed;
+    public GameObject rightDoor;
+    public GameObject rightDoorClosed;
+
+    private CustomButton buttonScript;
+    private bool initIncinerator = false;
+
+    private void Start()
+    {
+        buttonScript = button.GetComponent<CustomButton>();
+    }
+    private void Update()
+    {
+        if(buttonScript.pressed && syringe.transform.position == syringeSnapTo.transform.position && !initIncinerator)
+        {
+            if(leftDoor.transform.localEulerAngles.x != leftDoorClosed.transform.localEulerAngles.x && rightDoor.transform.localEulerAngles.x != rightDoorClosed.transform.localEulerAngles.x)
+            {
+                leftDoor.transform.localEulerAngles = new Vector3(leftDoor.transform.localEulerAngles.x + doorIncrement, leftDoor.transform.localEulerAngles.y, leftDoor.transform.localEulerAngles.z);
+                rightDoor.transform.localEulerAngles = new Vector3(rightDoor.transform.localEulerAngles.x + doorIncrement, rightDoor.transform.localEulerAngles.y, rightDoor.transform.localEulerAngles.z);
+                return;
+            }
+            else
+            {
+                Debug.Log("Doors at the same rotation");
+                StartCoroutine(InitIncinerator());
+                initIncinerator = true;
+            }
+        }
+    }
+    private IEnumerator InitIncinerator()
+    {
+        Debug.Log("Inside Coroutine");
+        yield return new WaitForSeconds(timeUntilLightsOn);
+        Debug.Log("Just Before Lights turn on");
+        lights.SetActive(true);
+    }
+}
